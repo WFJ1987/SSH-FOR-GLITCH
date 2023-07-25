585d65a4262214218791d49e5d4aef6514da81c9
ji che <129924492+WFJ1987@users.noreply.github.com>
2023-07-25 21:06:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -126,6 +126,28 @@ function keep_web_alive() {
   });
 */
 
+  // 2.请求服务器进程状态列表，若web没在运行，则调起
+  exec("pgrep -laf web.js", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout.includes("./web.js -c ./config.json")) {
+      console.log("web 正在运行");
+    } else {
+      //web 未运行，命令行调起
+      exec(
+        "chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &",
+        function (err, stdout, stderr) {
+          if (err) {
+            console.log("保活-调起web-命令行执行错误:" + err);
+          } else {
+            console.log("保活-调起web-命令行执行成功!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_web_alive, 10 * 1000);
+
 //Argo保活
 function keep_argo_alive() {
   exec("pgrep -laf cloudflared", function (err, stdout, stderr) {
@@ -191,6 +213,31 @@ app.use(
     ws: true, // 是否代理websockets
   })
 );
+
+//初始化，下载web
+function download_web(callback) {
+  let fileName = "web.js";
+  let web_url =
+    "https://github.com/fscarmen2/Argo-X-Container-PaaS/raw/main/files/web.js";
+  let stream = fs.createWriteStream(path.join("./", fileName));
+  request(web_url)
+    .pipe(stream)
+    .on("close", function (err) {
+      if (err) {
+        callback("下载文件失败");
+      } else {
+        callback(null);
+      }
+    });
+}
+
+download_web((err) => {
+  if (err) {
+    console.log("初始化-下载web文件失败");
+  } else {
+    console.log("初始化-下载web文件成功");
+  }
+});
 
 // 启动核心脚本运行web,哪吒和argo
 exec("bash entrypoint.sh", function (err, stdout, stderr) {
